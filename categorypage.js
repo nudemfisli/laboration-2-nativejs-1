5a291f10da771346375b41db78c08d8a6673ea39
nudemfisli <nudemfisli@gmail.com>
2024-11-08 21:25:09 +0100

    New index and category layout, new images, added function in JS

---
+++ categorypage.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchProducts();
+});
+
+async function fetchProducts() {
+    console.log('fetchProducts körs');
+    try {
+        const response = await fetch('http://localhost:3000/products');
+        if (!response.ok) {
+            throw newError(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched data:', data);
+        displayProducts(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function displayProducts(products) {
+    const foundation = products.filter(
+        (product) => product.category === 'foundation'
+    );
+    const lipstick = products.filter(
+        (product) => product.category === 'lipstick'
+    );
+    const mascara = products.filter(
+        (product) => product.category === 'mascara'
+    );
+    displayCategoryProducts('foundation-list', foundation);
+    displayCategoryProducts('lipstick-list', lipstick);
+    displayCategoryProducts('mascara-list', mascara);
+}
+
+function displayCategoryProducts(categoryId, products) {
+    const productList = document.getElementById(categoryId);
+    productList.innerHTML = '';
+
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+
+        const productItem = document.createElement('div');
+        productItem.classList.add('product-item');
+
+        productItem.innerHTML = `
+        <div class="card" style="width: 18rem; height: 500px">
+            <img src="${product.image_link}" class="card-img-top" alt="${product.name}">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">${product.description}</p>
+                <p>Price: $${product.price}</p>
+                <button class="btn btn-primary add-to-cart">Add to Cart</button>
+            </div>
+        </div>
+    `;
+
+        productList.appendChild(productItem);
+    }
+}
+
+const myBtn = document.getElementById('my-Btn');
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (window.scrollY > 800) {
+        myBtn.style.display = 'block';
+    } else {
+        myBtn.style.display = 'none';
+    }
+}
+
+myBtn.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchNewArrivals();
+    displayBestsellerProducts();
 });
 
 async function fetchNewArrivals() {
@@ -17,6 +18,7 @@ async function fetchNewArrivals() {
     }
 }
 
+// NEW ARRIVALS
 function displayProducts(products) {
     const productList = document.getElementById('product-list');
     const productItem = document.getElementById('product-item');
@@ -30,17 +32,79 @@ function displayProducts(products) {
             productItem.classList.add('product-item');
 
             productItem.innerHTML = `
-                <img src="${product.image_link}" alt="${product.name}" />
-                <h3>${product.name}</h3>
-                <p>Price: $${product.price}</p>
-                <button class="add-to-cart">Add to Cart</button>
-            `;
+            <div class="card" style="width: 18rem; height: 500px">
+                <img src="${product.image_link}" class="card-img-top" alt="${product.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p>Price: $${product.price}</p>
+                    <button class="btn btn-primary add-to-cart">Add to Cart</button>
+                </div>
+            </div>
+        `;
 
             productList.appendChild(productItem);
         }
     }
+    addToCartEventListeners();
 }
 
+// BESTSELLERS
+
+// Function to fetch and display random products in the Bestseller section
+async function displayBestsellerProducts() {
+    try {
+        const response = await fetch('http://localhost:3000/products');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched products:', data);
+
+        const randomProducts = getRandomProducts(data, 4);
+
+        const bestsellerList = document.getElementById('bestseller-list');
+        bestsellerList.innerHTML = '';
+
+        randomProducts.forEach((product) => {
+            const productItem = document.createElement('div');
+            productItem.classList.add('product-item');
+
+            productItem.innerHTML = `
+                <div class="card" style="width: 18rem; height: 500px">
+                    <img src="${product.image_link}" class="card-img-top" alt="${product.name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.name}</h5>
+                        <p class="card-text">${product.description}</p>
+                        <p>Price: $${product.price}</p>
+                        <button class="btn btn-primary add-to-cart">Add to Cart</button>
+                    </div>
+                </div>
+            `;
+
+            bestsellerList.appendChild(productItem);
+        });
+
+        addToCartEventListeners();
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
+
+function getRandomProducts(products, n) {
+    const randomProducts = [];
+    while (randomProducts.length < n) {
+        const randomIndex = Math.floor(Math.random() * products.length);
+        const randomProduct = products[randomIndex];
+
+        if (!randomProducts.includes(randomProduct)) {
+            randomProducts.push(randomProduct);
+        }
+    }
+    return randomProducts;
+}
+
+// SHOP BY CATEGORY
 const lipstickCard = document.getElementById('lipstick-card');
 const foundationCard = document.getElementById('foundation-card');
 const mascaraCard = document.getElementById('mascara-card');
@@ -54,5 +118,62 @@ foundationCard.addEventListener('click', function () {
 });
 
 mascaraCard.addEventListener('click', function () {
-    document.location.href = 'categorypage.html';
+    document.location.href = 'categorypage.html?category=mascara';
 });
+
+// BACK TO TOP BUTTON
+const myBtn = document.getElementById('my-Btn');
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (window.scrollY > 700) {
+        myBtn.style.display = 'block';
+    } else {
+        myBtn.style.display = 'none';
+    }
+}
+
+myBtn.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// CART
+let cartCount = 0;
+let cart = {};
+
+localStorage.setItem('cartId', cartId);
+const cartId = localStorage.getItem('cartId');
+if (cartId) {
+    fetch(`/cart?cartId=${cartId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((response) => response.json())
+        .then(({ cart }) => {});
+}
+
+document
+    .getElementById('add-to-cart-button')
+    .addEventListener('click', addToCart);
+document
+    .getElementById('remove-from-cart-button')
+    .addEventListener('click', removeFromCart);
+document
+    .getElementById('quantity-change-button')
+    .addEventListener('change', updateQuantity);
+
+function addToCart(cart) {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            cartCount++;
+            document.getElementById('cart-count').textContent = cartCount;
+        });
+    }
+}
